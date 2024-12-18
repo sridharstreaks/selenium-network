1ca0ca788475dd5a9648412770fbc8f634413b0b
Sridhar Streaks <100800059+sridharstreaks@users.noreply.github.com>
2024-12-18 21:37:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -39,12 +39,12 @@ def get_website_content(url):
 # ---------------- Page & UI/UX Components ------------------------
 def main_sidebar():
     # 1.Vertical Menu
-    st.header("Running Selenium on Streamlit Cloud")
+    st.header("Running Selenium Network on Streamlit Cloud")
     site_extraction_page()
 
 
 def site_extraction_page():
-    SAMPLE_URL = "https://spectrum.ieee.org/3d-printed-rocket"
+    SAMPLE_URL = "https://google.com"
     url = st.text_input(label="URL", placeholder="https://example.com", value=SAMPLE_URL)
 
     clicked = st.button("Load Page Content",type="primary")
